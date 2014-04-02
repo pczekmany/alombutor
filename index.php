21bbe016ec491c1ef3081671cbbabb85f1698b45
Molnár Zoltán <molnarzoli82@gmail.com>
2014-04-02 12:28:14 +0200

    - alap szerkezet

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
 session_start();
-include('parameters.php');
-#include('adatkapcsolat.inc.php');
-include('class/class.php');
-#######include('tartalomvalasztas.php');
-#include('email.php');
-#include('css_valaszt.php');
+require_once('class.php');
+require_once('tartalomvalasztas.php');
 
 $array = array('tartalom' => $tartalom,
 				'alcim' => $alcim,
 				'admin_konyvtar' => $admin_konyvtar);
 	 
 $index_html = new html_blokk;
-$index_html->load_template_file("template/index.html",$array);
-echo $index_html->html_code;
-?>
+
+echo $index_html->load_template_file("sablonok/index.html", $array);
