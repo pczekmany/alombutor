09098f749b1bd6f1d75de47c03545d778249af78
Peter <Peter@Peter-TOSH.t.hu>
2014-04-03 21:55:44 +0200

    termékoldal design

--- tartalomvalasztas.php
+++ tartalomvalasztas.php
@@ -3,11 +3,11 @@ $index_html = new html_blokk;
 $menu = filter_input(INPUT_GET, 'menu');
 
 $galeria = '
-<a href="termekkepek/ujtermek_kep.jpg" class="highslide" onclick="return hs.expand (this, {dimmingOpacity: 0.90})">
-    <img src="termekkepek/ujtermek_kep.jpg" alt="" />
+<a href="termekkepek/reka_sarok.jpg" class="highslide" onclick="return hs.expand (this, {dimmingOpacity: 0.90})">
+    <img src="termekkepek/reka_sarok.jpg" alt="" />
 </a>
 <div class="highslide-caption">
-    <div id="kepfelirat">Icuka első képe<br />(1/3)</div>
+    <div id="kepfelirat">Réka első képe<br />(1/3)</div>
     <a href="#" onclick="return hs.next(this)" class="controlleft">
     <img src="graphics/next_sz.png" border="0" alt="következő" title="következő" />
     </a>
@@ -16,11 +16,11 @@ $galeria = '
     </a>
 </div>
 
-<a href="termekkepek/akciostermek_kep.jpg" class="highslide" onclick="return hs.expand (this, {dimmingOpacity: 0.90})">
-    <img src="termekkepek/akciostermek_kep.jpg" alt="" />
+<a href="termekkepek/roma_sar_kon.jpg" class="highslide" onclick="return hs.expand (this, {dimmingOpacity: 0.90})">
+    <img src="termekkepek/roma_sar_kon.jpg" alt="" />
 </a>
 <div class="highslide-caption">
-    <div id="kepfelirat">Icuka második képe<br />(2/3)</div>
+    <div id="kepfelirat">Réka második képe<br />(2/3)</div>
     <a href="#" onclick="return hs.next(this)" class="controlleft">
     <img src="graphics/next_sz.png" border="0" alt="következő" title="következő" />
     </a>
@@ -29,8 +29,8 @@ $galeria = '
     </a>
 </div>
 
-<a href="termekkepek/akciostermek_kep.jpg" class="highslide" onclick="return hs.expand (this, {dimmingOpacity: 0.90})">
-    <img src="termekkepek/akciostermek_kep.jpg" alt="" />
+<a href="termekkepek/romakonh.jpg" class="highslide" onclick="return hs.expand (this, {dimmingOpacity: 0.90})">
+    <img src="termekkepek/romakonh.jpg" alt="" />
 </a>
 <div class="highslide-caption">
     <div id="kepfelirat">Icuka harmadik képe<br />(3/3)</div>
